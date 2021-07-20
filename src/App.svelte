1129429c7c0d7589fffa21a0ab385407e4d37718
roch25 <rochellemartins25@gmail.com>
2021-07-20 11:33:06 +0530

    Svelte + Vite setup

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
